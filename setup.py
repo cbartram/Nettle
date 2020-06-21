baf7a12e11f4192058c4f215b77b6184fff10fe2
cbartram <cbartram3@gmail.com>
2020-06-21 11:42:44 -0400

    Added version to python script and travis ci/cd

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 import setuptools
+import re
+
+VERSION_FILE="_version.py"
+verstrline = open(VERSION_FILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSION_FILE,))
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
       name='Nettle-cbartram',
-      version='1.0.0',
+      version=verstr,
       author='cbartram',
       author_email='cbartram3@gmail.com',
       url='https://github.com/cbartram/Nettle',
